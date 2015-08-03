10098d6e7dd8fc864939d6761be8f4d003f57219
onirix <romain.sandri@gmail.com>
2015-08-03 10:54:38 +0200

    Ajout du namespace dans les modèles de BDD Question et Choice

--- app/Choice.php
+++ app/Choice.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Choice extends Model
 {
-    //
+    use UserTrait, RemindableTrait;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'choices';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title','content', 'class_level', 'status'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    public function question()
+    {
+        return $this->belongsTo('ELycee\Question');
+    }
 }
--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    use UserTrait, RemindableTrait;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'questions';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title','content', 'class_level', 'status'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    public function choices()
+    {
+        return $this->hasMany('ELycee\Choices');
+    }
 }
